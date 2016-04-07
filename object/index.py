37e6659b372ad0e6b9239055868115017a364255
kevin72500 <kevin72500@qq.com>
2016-04-07 09:56:36 +0800

    Private variable and method, and how to use it

--- object/index.py
+++ object/index.py
@@ -23,10 +23,12 @@ class Province:
 
 	memo='中國23個省之一'#实例和类
 
-	def __init__(self,name,capital,leader):
+	def __init__(self,name,capital,leader,flag=False):
 		self.name=name
 		self.capital=capital
 		self.leader=leader
+
+		self.__Thailand=flag #私有字段
 
 	def sorts_meet(self):
 		print self.name+"开运动会"
@@ -39,17 +41,35 @@ class Province:
 	def getProp(self):
 		return "test"
 
+	def show(self):
+		print self.__Thailand
+
+	@property#调用私有变量
+	def getPrivateVari(self):
+		return self.show()
+
+	def __privMethod(self):#私有方法
+		print "我是私有的"
+
+	@property#调用私有方法
+	def getPrivateMeth(self):
+		return self.__privMethod()
+
 hb=Province("河北","石家莊","李洋")
 # sd=Province("山東","濟南","孔子")
+japan=Province("日本","九州","山本",True)
+japan.show()
+japan.getPrivateVari
+japan.getPrivateMeth
+japan._Province__privMethod()#直接调用私有方法
+# print hb.capital
+# hb.sorts_meet()
+# print hb.memo
+# hb.Foo()
+# print hb.getProp
+# print ("\n")
 
-print hb.capital
-hb.sorts_meet()
-print hb.memo
-hb.Foo()
-print hb.getProp
-print ("\n")
-
-print Province.memo
-Province.Foo()
+# print Province.memo
+# Province.Foo()
 #靜態類不嗯給你訪問靜態字段
 # print Province.name
